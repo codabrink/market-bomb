2bc3384c3845e34d5bf75475d37de6f5bc8c2dd4
Dakota Brink <git@dakota.io>
2021-09-25 16:03:25 -0400

    cleanup

---
+++ src/core.rs
@@ -0,0 +1,4 @@
+pub mod candle;
+pub use candle::Candle;
+pub mod strong_point;
+pub use strong_point::StrongPoint;
---
+++ src/core/strong_point.rs
@@ -0,0 +1,82 @@
+use crate::{prelude::*, CONFIG};
+use serde::Serialize;
+
+use CandlePos::*;
+
+#[derive(Serialize, Clone, Debug, PartialEq, Copy)]
+pub enum CandlePos {
+  HIGH,
+  LOW,
+  // OPEN,
+  // CLOSE,
+}
+impl From<CandlePos> for u8 {
+  fn from(cp: CandlePos) -> Self {
+    match cp {
+      HIGH => 1,
+      LOW => 0,
+    }
+  }
+}
+
+#[derive(Serialize, Clone, Copy, Debug, PartialEq)]
+pub struct StrongPoint {
+  pub position: CandlePos,
+  pub x: i64,
+  pub y: f32,
+  pub index: usize,
+  pub candle_index: usize,
+  pub domain: i32,
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct StrongPointConfig {
+  pub min_domain: u16,
+  pub count: u16,
+}
+
+impl StrongPoint {
+  pub fn new(
+    candles: &[Candle],
+    candle_index: usize,
+    position: CandlePos,
+    index: usize,
+  ) -> StrongPoint {
+    let candle = &candles[candle_index];
+
+    StrongPoint {
+      x: candle.open_time as i64,
+      y: match position {
+        CandlePos::HIGH => candle.high,
+        CandlePos::LOW => candle.low,
+      } as f32,
+      position,
+      domain: match position {
+        CandlePos::HIGH => candle.top_domain,
+        CandlePos::LOW => candle.bottom_domain,
+      },
+      candle_index,
+      index,
+    }
+  }
+}
+
+pub fn generate_points(candles: &[Candle]) -> Vec<StrongPoint> {
+  let min_domain = CONFIG.strong_points.min_domain;
+  let mut strong_points = vec![];
+  let mut index = 0;
+
+  for i in 0..candles.len() {
+    let candle = &candles[i];
+    if candle.top_domain >= min_domain {
+      strong_points.push(StrongPoint::new(candles, i, HIGH, index));
+      index += 1;
+    }
+    if candle.bottom_domain >= min_domain {
+      strong_points.push(StrongPoint::new(candles, i, LOW, index));
+      index += 1;
+    }
+  }
+
+  strong_points
+}
